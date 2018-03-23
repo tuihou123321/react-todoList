a0670d3b0c89bbbd6dfe2938e6f57de0816e4a61
tuihou123321 <chrome0423@gmail.com>
2018-03-23 08:41:34 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './static/css/common.css'
 import Item from "./components/Item"
 
 
@@ -8,26 +9,73 @@ class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            todosData:[1,2], //{id,value,hasCompleted}
-            inputValue:'',
+            todosData:[], //{id,value,hasCompleted}
+            inputValue:'11',
             view:'all' //要显示的视图
+        }
+        this.handleKeyDown=this.handleKeyDown.bind(this);
+        this.handleKeyUp=this.handleKeyUp.bind(this);
+        this.handleOnchange=this.handleOnchange.bind(this);
+        this.handleRemoveItem=this.handleRemoveItem.bind(this);
+        this.handleSelect=this.handleSelect.bind(this);
+    }
+    //选中效果
+    handleSelect(todo){
+        console.log(100);
+        let {todosData}=this.state;
+        todosData=todosData.filter((todo2)=>{
+            if(todo2.id===todo.id){
+                todo2.hasCompleted=!todo2.hasCompleted;
+            }
+            return true;
+        })
+        this.setState({todosData});
+    }
+    handleOnchange(e){
+        this.setState({inputValue:e.target.value})
+    }
+    handleKeyUp(e){
+        // console.log(e.target.value);
+    }
+    handleRemoveItem(todo){
+        let {todosData}=this.state;
+        todosData=todosData.filter((todo2)=>{
+            return todo2.id!==todo.id;
+        })
+        this.setState({todosData})
+    }
+    handleKeyDown(e){
+        let key=e.which || e.keyCode;
+        let todosData=this.state.todosData;
+        if(key===13){
+           todosData.push({
+                id:new Date().getTime(),
+                value:e.target.value,
+                hasCompleted:false
+            })
+            this.setState({todosData,inputValue:""})
         }
     }
   render() {
-    let Items=this.state.todosData.map((value,index)=>{
-        return <Item/>
-    })
+      let {handleKeyUp,handleKeyDown,handleOnchange,handleRemoveItem,handleSelect}=this
+      let {inputValue}=this.state
+
+      let Items = this.state.todosData.map((todo, index) => {
+          return <Item key={index} todo={todo} handleRemoveItem={handleRemoveItem} handleSelect={handleSelect}/>
+      })
     return (
-      <div className="container text-center">
+      <div className="container">
         <div className="row">
         <div className="col-md-6 col-md-offset-3">
-        <h1 style={{fontSize:"60px",color:"#ead7d7"}}>todos</h1>
+        <h1 style={{fontSize:"100px",color:"#ead7d7",marginBottom:"30px"}} className="text-center">todos</h1>
           <div>
               <div>
-                  <input type="text" className="form-control"/>
+                  <input type="text" className="form-control input" value={inputValue} onKeyUp={handleKeyUp} onKeyDown={handleKeyDown} onChange={handleOnchange}/>
               </div>
               <div>
-                  {Items}
+                  <ul style={{paddingLeft:"30px",paddingTop:"10px"}}>
+                    {Items}
+                  </ul>
               </div>
               <div>
                   底部
--- src/components/Item.js
+++ src/components/Item.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 
 
 class items extends Component {
+  constructor(){
+    super()
+    this.state={
+       value:1
+    }
+  }
+  //通过事件传递参数的方法：onClick={()=>fun1(todo)},而不是 onClick={fun1(todo)}
   render() {
+    //获取父组件传递过来的值，通过this.props来接收；
+    let {todo,handleRemoveItem,handleSelect}=this.props;
+    let selectedLi=todo.hasCompleted?"selectedLi":"";
     return (
-      <div>
-            我是子组件
-      </div>
+          <li style={{paddingBottom:"5px"}} className={selectedLi}>
+            <input type="checkbox" name="" id="" onChange={()=>{handleSelect(todo)}} />
+            <label style={{width:"80%"}}>{todo.value}</label>
+            <button onClick={()=>{handleRemoveItem(todo)}}>x</button>
+          </li>
     );
   }
 }
